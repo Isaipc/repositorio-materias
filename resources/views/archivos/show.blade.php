e6cdadce23bd9957ef3f09da91ebe381f3bcff11
Isai Polito <ispolitoc@gmail.com>
2022-04-06 19:29:45 -0500

    update archivo show view

--- resources/views/archivos/show.blade.php
+++ resources/views/archivos/show.blade.php
@@ -1,65 +1,44 @@
 @extends('layouts.dashboard')
 
-@section('breadcrumbs', Breadcrumbs::render('archivos.show', $item))
+@section('breadcrumbs', Breadcrumbs::render('archivos.show', $materia, $item))
 
-@section('title', 'Detalles de producto: ' . $item->nombre)
+@section('title', $item->nombre)
 
 @section('content')
-<table class="table table-striped table-sm">
-    <tbody>
-        <tr>
-            <th>Nombre</th>
-            <td> {{ $item->nombre }} </td>
-        </tr>
-        <tr>
-            <th>Categoria</th>
-            <td>
-                <a href="{{ route('materias.show', $item->categoria->id) }}">
-                    {{ $item->categoria->nombre }}
-                </a>
-            </td>
-        </tr>
-        <tr>
-            <th>Estatus</th>
-            <td> {{ $item->estatus ? 'Activo' : 'Inactivo'}} </td>
-        </tr>
-        <tr>
-            <th>Precio menudeo</th>
-            <td> $ {{ $item->precio_menudeo }} </td>
-        </tr>
-        <tr>
-            <th>Precio mayoreo</th>
-            <td> $ {{ $item->precio_mayoreo }} </td>
-        </tr>
-        <tr>
-            <th>Stock</th>
-            <td> {{ $item->stock }} </td>
-        </tr>
-        <tr>
-            <th>Detalles</th>
-            <td> {{ $item->detalles }} </td>
-        </tr>
-         <tr>
-            <th>Fecha de creación</th>
-            <td> {{ $item->created_at }} </td>
-        </tr>
-        <tr>
-            <th>Fecha de ultima actualización</th>
-            <td> {{ $item->updated_at }} </td>
-        </tr>
-    </tbody>
-</table>
-
-<label for="">Fotos</label>
-<div class="form-group row">
-    <div class="col-md-4">
-        @if ($item->images->count() == 0)
-        <img src="{{  asset('img/no-disponible.svg') }}" width="200px" alt="">
-        @else
-        <a href="{{ $item->images->first()->url }}">
-            <img src="{{ $item->images->first()->url }}" width="200px" class="img-thumbnail" alt="">
-        </a>
-        @endif
+<div class="row">
+    <div class="col-md-6">
+        <table class="table table-borderless">
+            <tbody>
+                <tr>
+                    <th class="text-right">Nombre</th>
+                    <td> {{ $item->nombre}} </td>
+                </tr>
+                <tr>
+                    <th class="text-right">Estatus</th>
+                    <td> {{ $item->getEstatusName() }} </td>
+                </tr>
+                <tr>
+                    <th class="text-right">URL</th>
+                    <td>
+                        <a href="{{ $item->url }}" class="btn btn-link">
+                            {{ $item->url }}
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <th class="text-right">Creado</th>
+                    <td> {{ $item->created_at }} </td>
+                </tr>
+                <tr>
+                    <th class="text-right">Actualizado</th>
+                    <td> {{ $item->updated_at }} </td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </div>
+@endsection
+
+@section('secondary-content')
+@include('archivos.list', $materia)
 @endsection
