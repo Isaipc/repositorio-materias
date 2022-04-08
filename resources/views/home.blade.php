d9851044dd9eb3b62a512ef798fd2aabc7374146
Isai Polito <ispolitoc@gmail.com>
2022-04-08 15:27:39 -0500

    update home view for admin & student

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,22 +15,17 @@ Mis materias
 <div class="d-flex flex-wrap justify-content-center">
     @hasrole('Administrador')
     @foreach ($materias_rep as $m)
-    <div class="card card-primary col-md-4 mr-2 mt-2">
-        <div class="card-body">
+    <div class="card card-primary col-md-4 ms-2 mt-2">
+        <div class="card-body text-center">
             <h5 class="card-title"> {{ $m->nombre }} </h5>
-
-            <div class="card-text">
-                No. alumnos: {{ $m->alumnos->count() }}
-            </div>
-
-            <div class="d-flex">
-                <a href="{{ route('archivos.index', $m)}}" class="btn btn-sm btn-link">
+            <div class="d-flex justify-content-around mt-2">
+                <a href="{{ route('archivos.index', $m)}}" class="btn btn-md btn-light">
                     <i class="bi bi-files"></i>
-                    Mostrar archivos
+                    Archivos ({{ $m->archivos->count() }})
                 </a>
-                <a href="{{ route('archivos.index', $m)}}" class="btn btn-sm btn-primary">
+                <a href="{{ route('materias.show', $m)}}" class="btn btn-md btn-light">
                     <i class="bi bi-people"></i>
-                    Mostrar alumnos
+                    Alumnos ({{ $m->alumnos->count() }})
                 </a>
             </div>
         </div>
@@ -39,19 +34,14 @@ Mis materias
     @endhasrole
 
     @foreach ($materias_alumn as $m)
-    <div class="card card-primary col-md-4 mr-2 mt-2">
-        <div class="card-body">
+    <div class="card card-primary col-md-4 ms-2 mt-2">
+        <div class="card-body text-center" >
             <h5 class="card-title"> {{ $m->nombre }} </h5>
-            <div class="card-text">
-                No. archivos: {{ $m->archivos->count() }}
-            </div>
 
-            <div class="row">
-                <a href="{{ route('materias.show', $m)}}" class="btn btn-sm btn-link">
-                    <i class="bi bi-files"></i>
-                    Mostrar archivos
-                </a>
-            </div>
+            <a href="{{ route('materias.show', $m)}}" class="btn btn-md btn-light">
+                <i class="bi bi-files"></i>
+                Archivos ({{ $m->archivos->count() }})
+            </a>
         </div>
     </div>
 
