f3dab34fa72f65e9258bfb5469a99c4d9ca96cf5
Isai Polito <ispolitoc@gmail.com>
2022-04-08 15:05:20 -0500

    update brand logo

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-md navbar-dark bg-primary shadow-sm ">
     <div class="container">
         <a class="navbar-brand text-uppercase font-weight-bold" href="{{ url('/') }}">
-            REP
+            <img src="{{ asset('img/isc.svg') }}" width="48px"  alt="">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
