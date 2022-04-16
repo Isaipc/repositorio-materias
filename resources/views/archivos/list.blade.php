c9bef347687f7802d7025ecee1428e73ed3e5ab5
Isai Polito <ispolitoc@gmail.com>
2022-04-16 13:00:07 -0500

    update archivos-list links

--- resources/views/archivos/list.blade.php
+++ resources/views/archivos/list.blade.php
@@ -15,7 +15,13 @@
                 @foreach ($rows as $key=> $r_item)
                 <tr class="">
                     <td>{{ $key + 1 }}</td>
-                    <td scope="row"> {{ $r_item->nombre }} </td>
+                    <td scope="row">
+                        <a href="{{ route('archivos.show', [$materia , $r_item->id]) }} " class="btn btn-md btn-link"
+                            data-bs-toggle="tooltip" data-bs-placement="top" title="Mostrar detalles">
+                            <i class="bi bi-box-arrow-up-right"></i>
+                        </a>
+                        {{ $r_item->nombre }}
+                    </td>
                     <td> <a href="{{ $r_item->url }}">{{ $r_item->url }}</a> </td>
                     <td>{{ $r_item->getEstatusName() }} </td>
                     <td>
@@ -40,8 +46,6 @@
                         </form>
 
                         @else
-                        <a href="{{ route('archivos.show', [$materia , $r_item->id]) }} " class="btn btn-sm btn-link">
-                            Mostrar detalles </a>
                         <a href="{{ route('archivos.edit',[$materia, $r_item]) }} " class="btn btn-sm btn-primary">
                             <i class="bi bi-pencil-fill"></i> </a>
                         <a href="javascript: document.getElementById('archive-{{ $r_item->id }}').submit()"
