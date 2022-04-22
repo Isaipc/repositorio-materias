643fd4ce450b021bde54c6b1c45f0640f2a37bb9
Isai Polito <ispolitoc@gmail.com>
2022-04-22 02:34:15 -0500

    style: change bs classes

--- resources/views/materias/create.blade.php
+++ resources/views/materias/create.blade.php
@@ -6,14 +6,14 @@
 @section('primary-content')
 <form action="{{ route('materias.store') }} " method="POST">
     @csrf
-    <div class="form-group row">
+    <div class="row mb-3">
         <div class="col-md-4">
             <label for="nom" class="col-form-label text-md-right">Nombre</label>
             <input id="nom" type="text" class="form-control" name="nombre" maxlength="100" required
                 value="{{ old('nombre') }}">
         </div>
     </div>
-    <div class="form-group row">
+    <div class="row mb-3">
         <div class="col-md-4">
             <div class="form-check form-switch">
                 <input class="form-check-input" name="estatus" type="checkbox" role="switch" id="est">
@@ -24,7 +24,6 @@
     <button type="submit" class="btn btn-md btn-primary">Guardar</button>
     <a href="{{ route('materias.index') }} " class="btn btn-md btn-light">Cancelar</a>
 </form>
-
 @endsection
 
 @section('secondary-content')
