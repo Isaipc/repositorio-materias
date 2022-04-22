21afdd863edf39ff6e2eaef3fd6eb265f673e440
Isai Polito <ispolitoc@gmail.com>
2022-04-21 23:41:13 -0500

    add view composer for materia views

---
+++ app/Providers/MateriasProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use App\Materia;
+use Illuminate\Support\ServiceProvider;
+
+class MateriasProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+        view()->composer('*', function ($view) {
+            $view->with('archived', Materia::archived()->get())
+                ->with('rows', Materia::actives()->get());
+        });
+    }
+}
