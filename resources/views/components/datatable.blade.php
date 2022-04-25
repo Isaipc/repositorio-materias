1153be8d912250b521f163140097904ba4467c8e
Isai Polito <ispolitoc@gmail.com>
2022-04-25 16:54:07 -0500

    update datatable component

--- resources/views/components/datatable.blade.php
+++ resources/views/components/datatable.blade.php
@@ -1,8 +1,5 @@
-<table class="table table-hover table-responsive-md table-md mt-2 datatable">
+<table id="{{ $id }}"" class="table table-hover table-responsive-md table-md mt-2 datatable">
     <thead>
         {{ $thead }}
     </thead>
-    <tbody>
-        {{ $tbody }}
-    </tbody>
 </table>
