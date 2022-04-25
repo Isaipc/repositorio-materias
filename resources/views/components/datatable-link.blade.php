67a17e050b0c81f5c27e08febc5c4f2e9f152952
Isai Polito <ispolitoc@gmail.com>
2022-04-25 16:54:56 -0500

    remove unnecessary blade components

--- resources/views/components/datatable-link.blade.php
+++
@@ -1,4 +0,0 @@
-<a href="{{ route($link, $id) }} " class="btn btn-link" data-bs-toggle="tooltip"
-    data-bs-placement="top" title="Mostrar detalles">
-    <i class="bi bi-box-arrow-up-right"></i>
-</a>
