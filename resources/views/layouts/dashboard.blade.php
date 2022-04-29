703e5e2346b63a75284509eb586eadf670d2ff67
Isai Polito <ispolitoc@gmail.com>
2022-04-29 15:29:10 -0500

    format dashboard layout

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -2,40 +2,40 @@
 
 @section('frame')
 
-@include('navbar')
+    @include('navbar')
 
-<main class="py-2">
-    <div class="container">
-        <div class="mt-3">
-            @yield('breadcrumbs')
-        </div>
-
-        <div class="card mt-3 shadow-sm">
-            <div class="card-header bg-primary text-white text-center text-uppercase">
-                @yield('primary-title')
+    <main class="py-2">
+        <div class="container">
+            <div class="mt-3">
+                @yield('breadcrumbs')
             </div>
-            <div class="card-body container">
-                @if ($errors->any())
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
+
+            <div class="card mt-3 shadow-sm">
+                <div class="card-header bg-primary text-white text-center text-uppercase">
+                    @yield('primary-title')
                 </div>
-                @endif
-                @yield('primary-content')
+                <div class="card-body container">
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+                    @yield('primary-content')
+                </div>
             </div>
-        </div>
-        <div class="card mt-3 shadow-sm">
-            <div class="card-header bg-light text-dark text-center text-uppercase">
-                @yield('secondary-title')
+            <div class="card mt-3 shadow-sm">
+                <div class="card-header bg-light text-dark text-center text-uppercase">
+                    @yield('secondary-title')
+                </div>
+                <div class="card-body container">
+                    @yield('secondary-content')
+                </div>
             </div>
-            <div class="card-body container">
-                @yield('secondary-content')
-            </div>
-        </div>
 
-    </div>
-</main>
+        </div>
+    </main>
 @endsection
