76a17b442f2e8f4fdb5084903cf6831a08ee11e6
Isai Polito <ispolitoc@gmail.com>
2022-04-29 04:31:48 -0500

    add exceptions handler to render

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,7 +3,9 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
 
 class Handler extends ExceptionHandler
 {
@@ -46,6 +48,67 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+
+        // is this request asks for json?
+        if ($request->header('Content-Type') == 'application/json') {
+
+            /*  is this exception? */
+
+            if (!empty($exception)) {
+
+                // set default error message
+                $response = [
+                    'error' => 'Sorry, can not execute your request.'
+                ];
+
+                // If debug mode is enabled
+                if (config('app.debug')) {
+                    // Add the exception class name, message and stack trace to response
+                    $response['exception'] = get_class($exception); // Reflection might be better here
+                    $response['message'] = $exception->getMessage();
+                    $response['trace'] = $exception->getTrace();
+                }
+
+                $status = 400;
+
+                // get correct status code
+
+                // is this validation exception
+                if ($exception instanceof ValidationException) {
+
+                    return $this->convertValidationExceptionToResponse($exception, $request);
+
+                    // is it authentication exception
+                } else if ($exception instanceof AuthenticationException) {
+
+                    $status = 401;
+
+                    $response['error'] = 'Can not finish authentication!';
+
+                    //is it DB exception
+                } else if ($exception instanceof \PDOException) {
+
+                    $status = 500;
+
+                    $response['error'] = 'Can not finish your query request!';
+
+                    // is it http exception (this can give us status code)
+                } else if ($this->isHttpException($exception)) {
+
+                    $status = $exception->getStatusCode();
+
+                    $response['error'] = 'Request error!';
+                } else {
+
+                    // for all others check do we have method getStatusCode and try to get it
+                    // otherwise, set the status to 400
+                    $status = method_exists($exception, 'getStatusCode') ? $exception->getStatusCode() : 400;
+                }
+
+                return response()->json($response, $status);
+            }
+        }
+
         return parent::render($request, $exception);
     }
 }
