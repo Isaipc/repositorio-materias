4c0d468291dfe2c539fa1823a53d8882e5fe7b3f
Isai Polito <ispolitoc@gmail.com>
2022-05-06 00:46:14 -0500

    add function to generate random strings

--- public/js/functions.js
+++ public/js/functions.js
@@ -48,3 +48,14 @@ function showToast(msg, type) {
 function getSwitchStatus(status) {
     return status == STATUS_ENABLED;
 }
+
+function generateRandomKey(length = 5) {
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() *
+            charactersLength));
+    }
+    return result;
+}
