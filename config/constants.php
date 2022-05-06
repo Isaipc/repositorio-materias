4afa07f40cfe3176c66ed14c52db5004eecc461a
Isai Polito <ispolitoc@gmail.com>
2022-05-06 18:10:22 -0500

    update constants config

--- config/constants.php
+++ config/constants.php
@@ -4,4 +4,8 @@ return [
     'pag_size' => 10,
     'timestamps_format' => 'd/m/Y h:i A',
     'output_date_format' => 'd/m/Y',
+    'key_length' => 5,
+    'status_archived' => 0,
+    'status_enabled' => 1,
+    'status_disabled' => 2
 ];
