6132cde22180431b0bb09740b7340f40156da408
Isai Polito <ispolitoc@gmail.com>
2022-05-09 17:14:29 -0500

    feat: reset passwords

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,43 +1,38 @@
-@extends('layouts.dashboard')
-
-@section('title', 'Restablecer contraseña')
+@extends('layouts.guest')
 
 @section('content')
-<div class="row justify-content-center">
-    <div class="col-md-8">
+    <div class="row justify-content-center">
+
+        <h4 class="text-center mb-4">Restrablecer contraseña</h4>
 
         @if (session('status'))
-        <div class="alert alert-success" role="alert">
-            {{ session('status') }}
-        </div>
+            <div class="alert alert-success" role="alert">
+                {{ session('status') }}
+            </div>
         @endif
 
         <form method="POST" action="{{ route('password.email') }}">
             @csrf
 
-            <div class="form-group row">
-                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Correo electrónico') }}</label>
+            <div class="mb-3">
+                <label for="email" class="col-form-label text-md-right">{{ __('Correo electrónico') }}</label>
 
-                <div class="col-md-6">
-                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
-                        name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email"
+                    value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                    @error('email')
+                @error('email')
                     <span class="invalid-feedback" role="alert">
                         <strong>{{ $message }}</strong>
                     </span>
-                    @enderror
-                </div>
+                @enderror
             </div>
 
-            <div class="form-group row mb-0">
-                <div class="col-md-6 offset-md-4">
-                    <button type="submit" class="btn btn-primary">
-                        {{ __('Enviar enlace') }}
-                    </button>
-                </div>
+            <div class="mb-3">
+                <button type="submit" class="btn btn-primary">
+                    {{ __('Enviar enlace') }}
+                </button>
+                <a href="{{ route('login') }}" class="btn btn-link">Cancelar</a>
             </div>
         </form>
     </div>
-</div>
 @endsection
