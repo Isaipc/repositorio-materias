1cf733077d0e44b952b150ff54de6ebf4edb0cc2
Isai Polito <ispolitoc@gmail.com>
2022-06-05 19:15:27 -0500

    add new disk

--- config/filesystems.php
+++ config/filesystems.php
@@ -54,7 +54,11 @@ return [
             'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
         ],
-
+        'files' => [
+            'driver' => 'local',
+            'root' => storage_path('app/files'),
+            'visibility' => 'private'
+        ],
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
