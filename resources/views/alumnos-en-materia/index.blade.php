93e4ca590b7b6d90267e17f76bb07a8d963bbb34
Isai Polito <ispolitoc@gmail.com>
2022-07-09 20:06:41 -0500

    refactor alumnos-en-materia index view

---
+++ resources/views/alumnos-en-materia/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.dashboard')
+
+@section('breadcrumbs', Breadcrumbs::render('alumnos.index', $item))
+
+@section('primary-title')
+    <i class="bi bi-people-fill"></i>
+    {{ __('Alumnos en materias') }}
+@endsection
+
+@section('primary-content')
+    <input id="materiaId" type="hidden" name="materia_id" value="{{ $item->id }}">
+    @datatable(['id' => 'table'])
+    @slot('thead')
+        <tr>
+            <th>Alumno</th>
+            <th>Correo</th>
+            <th>Registrado</th>
+        </tr>
+    @endslot
+    @enddatatable
+@endsection
+
+@section('scripts')
+    <script src="{{ asset('js/alumnos-en-materia.js')}}"></script>
+@endsection
