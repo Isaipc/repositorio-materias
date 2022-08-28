485bd6870753fa4c7e569198eca38e4a9c93300c
Isai Polito <ispolitoc@gmail.com>
2022-08-27 21:56:54 -0500

    add validation elements to materias form

--- resources/views/materias/index.blade.php
+++ resources/views/materias/index.blade.php
@@ -25,7 +25,7 @@
     <!-- Save Modal -->
     <div class="modal fade" id="materiaModal" tabindex="-1" role="dialog" aria-labelledby="modelTitleId"
         aria-hidden="true">
-        <form id="materiaForm" action="javascript:void(0)" method="POST">
+        <form id="materiaForm" action="javascript:void(0)" method="POST" class="needs-validation" novalidate>
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
@@ -33,11 +33,12 @@
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
+                        <div id="error" class="alert alert-danger d-none"></div>
                         <input id="materiaId" type="hidden" name="id" value="0">
                         <div class="mb-3">
                             <label for="nombre" class="form-label text-md-right">Nombre</label>
-                            <input id="nombre" type="text" class="form-control" name="nombre" maxlength="100" required
-                                value="{{ old('nombre') }}">
+                            <input id="nombre" type="text" class="form-control" name="nombre" maxlength="60" required>
+                            <ul id="nombreInvalidFeedback" class="invalid-feedback"></ul>
                         </div>
                         <div class="mb-3">
                             <div class="form-check form-switch">
@@ -48,9 +49,9 @@
                         </div>
                         <label for="clave" class="form-label text-md-right">Clave</label>
                         <div class="input-group mb-3">
-                            <input id="clave" type="text" class="form-control" name="clave" maxlength="100"
-                                aria-label="Example text with button addon" value="{{ old('clave') }}"
-                                aria-describedby="buttonRandomKey">
+                            <input id="clave" type="text" class="form-control" name="clave" maxlength="10"
+                                aria-label="Example text with button addon" aria-describedby="buttonRandomKey" required>
+                            <ul id="claveInvalidFeedback" class="invalid-feedback"></ul>
                             <button class="btn btn-outline-secondary" type="button" id="buttonRandomKey">Generar
                                 clave</button>
                         </div>
